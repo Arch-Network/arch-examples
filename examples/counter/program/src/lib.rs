123ac2e1cfbab7a9e607429d07fbf4ed5dc7ca75
GitHub Action <action@github.com>
2025-01-30 12:30:15 +0000

    Sync examples from main repository

--- examples/counter/program/src/lib.rs
+++ examples/counter/program/src/lib.rs
@@ -1,11 +1,25 @@
-use std::str::FromStr;
 use arch_program::{
-    account::{AccountInfo, AccountMeta}, bitcoin::{self, absolute::LockTime, transaction::Version, Address, Amount, ScriptBuf, Transaction, TxOut}, entrypoint, helper::add_state_transition, input_to_sign::InputToSign, instruction::Instruction, msg, program::{
+    account::{AccountInfo, AccountMeta},
+    bitcoin::{
+        self, absolute::LockTime, transaction::Version, Address, Amount, ScriptBuf, Transaction,
+        TxOut,
+    },
+    entrypoint,
+    helper::add_state_transition,
+    input_to_sign::InputToSign,
+    instruction::Instruction,
+    msg,
+    program::{
         get_account_script_pubkey, get_bitcoin_block_height, invoke, next_account_info,
         set_transaction_to_sign,
-    }, program_error::ProgramError, pubkey::Pubkey, system_instruction::SystemInstruction, transaction_to_sign::TransactionToSign, utxo::UtxoMeta
+    },
+    program_error::ProgramError,
+    pubkey::Pubkey,
+    transaction_to_sign::TransactionToSign,
+    utxo::UtxoMeta,
 };
 use borsh::{BorshDeserialize, BorshSerialize};
+use std::str::FromStr;
 
 entrypoint!(process_instruction);
 pub fn process_instruction(
@@ -22,7 +36,13 @@ pub fn process_instruction(
         .map_err(|_e| ProgramError::AccountBorrowFailed)?
         .len();
 
-    msg!("account data : {:?}", account.data.try_borrow().map_err(|_e| ProgramError::Custom(501))?);
+    msg!(
+        "account data : {:?}",
+        account
+            .data
+            .try_borrow()
+            .map_err(|_e| ProgramError::Custom(501))?
+    );
 
     let counter_input: CounterInput =
         borsh::from_slice(instruction_data).map_err(|_e| ProgramError::InvalidArgument)?;
@@ -54,7 +74,10 @@ pub fn process_instruction(
                 .map_err(|_e| ProgramError::AccountBorrowFailed)?
                 .copy_from_slice(&serialized_counter_data);
 
-            msg!(&format!("Mutating memory ! {:?}", serialized_counter_data.len()));
+            msg!(&format!(
+                "Mutating memory ! {:?}",
+                serialized_counter_data.len()
+            ));
         }
         CounterInstruction::IncreaseCounter => {
             if data_len == 0 {
@@ -69,17 +92,23 @@ pub fn process_instruction(
             let counter_data: CounterData = borsh::from_slice(&serialized_current_counter_data)
                 .map_err(|_e| ProgramError::InvalidAccountData)?;
 
+            let new_counter_data = CounterData::new(
+                counter_data.current_value + counter_data.current_step,
+                counter_data.current_step,
+            );
 
-            let new_counter_data = CounterData::new(counter_data.current_value + counter_data.current_step, counter_data.current_step);
+            let new_data =
+                borsh::to_vec(&new_counter_data).map_err(|_e| ProgramError::Custom(502))?;
 
-            let new_data = borsh::to_vec(&new_counter_data).map_err(|_e| ProgramError::Custom(502))?;
-
-            msg!(&format!("Increasing counter ! {:?} new data {:?}", counter_data,new_data));
+            msg!(&format!(
+                "Increasing counter ! {:?} new data {:?}",
+                counter_data, new_data
+            ));
 
             if new_data.len() > data_len {
                 account.realloc(new_data.len(), true)?;
             }
-            
+
             msg!(&format!("Reallocated memory ! {:?}", new_data.len()));
 
             drop(serialized_current_counter_data);
@@ -90,13 +119,14 @@ pub fn process_instruction(
                 .copy_from_slice(&new_data);
 
             msg!("Successfully increased counter !");
-        },
+        }
     }
 
-    if counter_input.anchoring.is_some(){
+    if counter_input.anchoring.is_some() {
         let (utxo, serialized_tx, anchoring_should_fail) = counter_input.anchoring.unwrap();
 
-        let fees_tx: Transaction = bitcoin::consensus::deserialize(&serialized_tx).map_err(|_e| ProgramError::Custom(504))?;
+        let fees_tx: Transaction = bitcoin::consensus::deserialize(&serialized_tx)
+            .map_err(|_e| ProgramError::Custom(504))?;
 
         let mut tx = Transaction {
             version: Version::TWO,
@@ -114,16 +144,18 @@ pub fn process_instruction(
         if !anchoring_should_fail {
             tx.input.push(fees_tx.input[0].clone());
 
-            let script_buff = Address::from_str("bcrt1q9lu00cj3y0qzm6wqr6nr46s877259uz9r802sm").map_err(|_e| ProgramError::Custom(505))?.assume_checked().script_pubkey();
-            
-            if counter_input.add_output.is_some(){
+            let script_buff = Address::from_str("bcrt1q9lu00cj3y0qzm6wqr6nr46s877259uz9r802sm")
+                .map_err(|_e| ProgramError::Custom(505))?
+                .assume_checked()
+                .script_pubkey();
+
+            if counter_input.add_output.is_some() {
                 let amount: u64 = counter_input.add_output.unwrap();
 
-                tx.output.push(TxOut{
+                tx.output.push(TxOut {
                     value: Amount::from_sat(amount),
-                    script_pubkey: script_buff
+                    script_pubkey: script_buff,
                 })
-
             }
         }
         let tx_to_sign = TransactionToSign {
@@ -133,19 +165,19 @@ pub fn process_instruction(
                 signer: account.key.clone(),
             }],
         };
-    
+
         msg!("Transaction to sign {:?}", tx);
         set_transaction_to_sign(accounts, tx_to_sign)?
     }
 
-    if counter_input.should_panic{
+    if counter_input.should_panic {
         panic!("PANICKED BY REQUEST");
     }
 
-    if counter_input.should_return_err{
-        return Err(ProgramError::Custom(1))
+    if counter_input.should_return_err {
+        return Err(ProgramError::Custom(1));
     }
-    
+
     Ok(())
 }
 
