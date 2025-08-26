ae6f0f1f9a034aaf1adedf5718bdf9f47b2e97d1
GitHub Action <action@github.com>
2025-08-26 00:19:48 +0000

    Sync examples from main repository

--- examples/duplicate_signed_txs/src/lib.rs
+++ examples/duplicate_signed_txs/src/lib.rs
@@ -5,27 +5,26 @@ use bitcoin::XOnlyPublicKey;
 use arch_program::account::MIN_ACCOUNT_LAMPORTS;
 use arch_program::pubkey::Pubkey;
 use arch_program::system_instruction;
-use arch_sdk::{build_and_sign_transaction, generate_new_keypair, ArchRpcClient};
-use arch_test_sdk::{
-    constants::{BITCOIN_NETWORK, NODE1_ADDRESS},
-    helper::{create_and_fund_account_with_faucet, send_transactions_and_wait},
-};
+use arch_sdk::{build_and_sign_transaction, generate_new_keypair, ArchRpcClient, Config};
 
 #[ignore]
 #[test]
 #[should_panic]
 fn poc_duplicate_signed_txs() {
-    let client = ArchRpcClient::new(NODE1_ADDRESS);
+    let config = Config::localnet();
+    let client = ArchRpcClient::new(&config.arch_node_url);
 
-    let (authority_keypair, authority_pubkey, _) = generate_new_keypair(BITCOIN_NETWORK);
-    create_and_fund_account_with_faucet(&authority_keypair, BITCOIN_NETWORK);
+    let (authority_keypair, authority_pubkey, _) = generate_new_keypair(config.network);
+    client
+        .create_and_fund_account_with_faucet(&authority_keypair, config.network)
+        .unwrap();
 
     let fee_payer_pubkey = Pubkey::from_slice(
         &XOnlyPublicKey::from_keypair(&authority_keypair)
             .0
             .serialize(),
     );
-    let (account_keypair, account_pubkey, _) = generate_new_keypair(BITCOIN_NETWORK);
+    let (account_keypair, account_pubkey, _) = generate_new_keypair(config.network);
     println!("Account created with address, {:?}", account_pubkey.0);
 
     let tx = build_and_sign_transaction(
@@ -41,9 +40,10 @@ fn poc_duplicate_signed_txs() {
             client.get_best_finalized_block_hash().unwrap(),
         ),
         vec![authority_keypair, account_keypair],
-        BITCOIN_NETWORK,
+        config.network,
     )
     .expect("Failed to build and sign transaction");
 
-    let _processed_tx = send_transactions_and_wait(vec![tx.clone(), tx]);
+    let txids = client.send_transactions(vec![tx.clone(), tx]).unwrap();
+    let _processed_tx = client.wait_for_processed_transactions(txids);
 }
