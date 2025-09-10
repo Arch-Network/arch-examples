76ca4c51cd1bb3dda7aafe49a7a9970bc2dc80ff
GitHub Action <action@github.com>
2025-09-10 18:08:47 +0000

    Sync examples from main repository

---
+++ examples/test-sol-log-data/src/lib.rs
@@ -0,0 +1,33 @@
+use arch_program::{
+    account::AccountInfo, entrypoint, entrypoint::ProgramResult, log, msg, pubkey::Pubkey,
+};
+
+entrypoint!(process_instruction);
+
+pub fn process_instruction(
+    _program_id: &Pubkey,
+    _accounts: &[AccountInfo],
+    _instruction_data: &[u8],
+) -> ProgramResult {
+    // Test basic logging
+    msg!("Testing sol_log_data syscall");
+
+    // Test logging binary data
+    let test_data = b"Hello, World! This is binary data.";
+    log::sol_log_data(test_data);
+
+    // Test logging different types of data
+    let hex_data = b"0x1234567890abcdef";
+    log::sol_log_data(hex_data);
+
+    // Test logging empty data
+    log::sol_log_data(b"");
+
+    // Test logging large data
+    let large_data = vec![0u8; 100];
+    log::sol_log_data(&large_data);
+
+    msg!("sol_log_data test completed successfully");
+
+    Ok(())
+}
